b1cf0c343c0ca89e4514a7344738b14653a67977
Phil <peva0411@gmail.com>
2014-02-25 22:05:37 -0500

    added result view and basic game flow

--- PhonePhrase/Angular/main.js
+++ PhonePhrase/Angular/main.js
@@ -7,49 +7,90 @@
     .when('/round', {
         templateUrl: '/Angular/views/round.html',
         controller: 'RoundController'
-     })
+    })
+    .when('/results', {
+        templateUrl: '/Angular/views/results.html',
+        controller: 'ResultsController'
+        })
     .otherwise({
         redirectTo:'/'
     });
 }]);
 
-app.factory('game', ['audio', '$rootScope', function(audio, $rootScope) {
+app.factory('audio', [
+    '$document', function($document) {
+        var timerAudio = $document[0].createElement('audio');
+        timerAudio.src = '/Content/Sounds/TicTock.mp3';
+        timerAudio.loop = true;
+        var buzzerAudio = $document[0].createElement('audio');
+        buzzerAudio.src = '/Content/Sounds/Buzzer.mp3';
+
+        return {
+            buzzer: buzzerAudio,
+            timer: timerAudio
+        };
+    }
+]);
+
+
+app.factory('game', ['$timeout', '$rootScope', 'audio', function($timeout, $rootScope, audio) {
     var game = {
-        redTeamScore: 0,
-        blueTeamScore: 0,
-        roundCount: 0,
-        newRound: function() {
-            return {
-                time: 5000,
-                active: true,
-                words:["cat", "dog", "bird"],
-                start: function() {
-                    var promise = $interval()
-                }
-        }
-        }
-};
+            audio: audio,
+            redTeamScore: 0,
+            blueTeamScore: 0,
+            roundCount: 0,
+            roundOver: false,
+            gameIsActive: false,
+            words: ["cat", "dog", "mouse", "Elephant", "Cow", "Duck", "Horse", "Bird"],
+            startRound: function () {
+                    game.roundOver = false;
+                    game.audio.timer.play();
+                    $timeout(function() {
+                        game.roundOver = true;
+                        game.audio.timer.pause();
+                    }, 5000);
+            },
+            getNextWord: function() {
+                game.currentWord = game.words.pop();
+            }
+        };
 
-
-}]);
+        return game;
+    }
+]);
 
 app.controller('StartController', [
-    "$scope", "$location", function($scope, $location) {
-        $scope.start = function() {
+    "$scope", "$location", "game", function($scope, $location, game) {
+        $scope.start = function () {
+            game.startRound();
             $location.path('/round');
         };
     }
 ]);
 
 app.controller('RoundController', [
-    "$scope", function($scope) {
+    "$scope", 'game', '$location', function($scope, game, $location) {
+        $scope.game = game;
 
-        $scope.words = ["dog", "cat", "zebra"];
-        $scope.currentWord = "";
+        $scope.$watch('game.roundOver', function(value) {
+            if (value) {
+                game.audio.buzzer.play();
+                $location.path('/results');
+            }
+        });
+    }
+]);
 
-        $scope.getNextWord = function() {
-            //pop word from words array 
-            $scope.currentWord = $scope.words.pop();
+app.controller('ResultsController', [
+    '$scope', '$location', 'game', function($scope,$location, game) {
+        $scope.redWon = function() {
+            game.redTeamScore++;
+            $location.path('/');
+        };
+
+        $scope.blueWon = function() {
+            game.blueTeamScore++;
+            $location.path('/');
         };
     }
 ]);
