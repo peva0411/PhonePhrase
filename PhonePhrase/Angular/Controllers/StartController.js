086897223e8fb6b417dd55934530fdaf49a4c1e6
Phil <peva0411@gmail.com>
2014-04-05 18:55:37 -0400

    added muted play for mobile testing

--- PhonePhrase/Angular/Controllers/StartController.js
+++ PhonePhrase/Angular/Controllers/StartController.js
@@ -1,6 +1,16 @@
 ﻿angular.module('phraseApp').controller('StartController', [
     "$scope", "$location", "game", "audio", function ($scope, $location, game, audio) {
         $scope.start = function () {
+
+            audio.timer.muted = true;
+            audio.buzzer.muted = true;
+            audio.timer.play();
+            audio.timer.pause();
+            audio.buzzer.play();
+            audio.buzzer.pause();
+            audio.timer.muted = false;
+            audio.buzzer.muted = false;
+
             game.round.startRound();
             $location.path('/round');
         };
